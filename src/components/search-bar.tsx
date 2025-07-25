defff6c086d0b13c7bc9b12b435b72570d96381c
Mariano Capiliku <marianocapiliku@gmail.com>
2025-07-25 23:18:16 +0100

    Created functionality to clear search query

--- src/components/search-bar.tsx
+++ src/components/search-bar.tsx
@@ -1,4 +1,4 @@
-import { SearchIcon } from "lucide-react";
+import { CircleX, SearchIcon } from "lucide-react";
 import { useRouter } from "next/router";
 import { useCallback } from "react";
 import { cn } from "@/lib/utils";
@@ -6,7 +6,7 @@ import { Input } from "./ui/input";
 
 const SeachBar = () => {
 	const router = useRouter();
-	const query = router.query.q as string;
+	const query = (router.query.q as string) ?? "";
 
 	const handleSearch = useCallback(
 		(event: React.FormEvent) => {
@@ -21,8 +21,14 @@ const SeachBar = () => {
 
 	const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
 		const newQuery = event.target.value;
-
 		router.push(`/blog?q=${encodeURIComponent(newQuery)}`, undefined, {
+			shallow: true,
+			scroll: false,
+		});
+	};
+
+	const clearQuery = () => {
+		router.push("/blog", undefined, {
 			shallow: true,
 			scroll: false,
 		});
@@ -33,6 +39,7 @@ const SeachBar = () => {
 			<Input
 				type="text"
 				placeholder="Pesquisar..."
+				value={query}
 				icon={
 					<SearchIcon
 						className={cn(
@@ -44,6 +51,12 @@ const SeachBar = () => {
 				className="h-10 w-full  md:w-72 ng-transparent border border-gray-400 pl-9 text-gray-100 outline-none transition-all duration-200 focus-within:border-blue-300 focus-within:ring-1 focus-within:ring-blue-300 placeholder:text-body-sm placeholder:text-gray-300 "
 				onChange={handleQueryChange}
 			/>
+			{query && (
+				<CircleX
+					onClick={clearQuery}
+					className="absolute right-3 top-1/2 h-4 w-4 -translate-y-1/2 text-gray-300 transition-colors duration-200 hover:cursor-pointer hover:text-red-400 "
+				/>
+			)}
 		</form>
 	);
 };
