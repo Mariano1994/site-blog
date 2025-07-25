21a3b226c48dc0748c1d80a63c5adecd660baaf2
Mariano Capiliku <marianocapiliku@gmail.com>
2025-07-25 12:19:20 +0100

    Start building the blog page, created header component ui

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,7 +1,18 @@
 const Blog = () => {
 	return (
-		<div className="flex items-center justify-center h-screen bg-red-300">
-			<span>Blog Page</span>
+		<div className="flex flex-col py-24 flex-grow h-full">
+			<header>
+				<div className="container space-y6 flex flex-col items-start justify-between md:flex-row md:items-end lg:items-end ">
+					<div className="flex flex-col gap-4 ">
+						<span className="text-body-tag text-cyan-100 w-fit rounded-sm text-center md:text-left py-2 px-4 bg-cyan-300">
+							BLOG
+						</span>
+						<h1 className="text-balance text-start md:text-left text-heading-lg md:text-heading-xl text-gray-100 max-w-2xl">
+							Dicas e estrategias para impulsionar o seu negócio
+						</h1>
+					</div>
+				</div>
+			</header>
 		</div>
 	);
 };
