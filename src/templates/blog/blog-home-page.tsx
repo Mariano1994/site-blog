8ac0ca196c111cd2096095dc271847a97103c31f
Mariano Capiliku <marianocapiliku@gmail.com>
2025-07-27 21:48:37 +0100

    created grid card component

--- src/templates/blog/blog-home-page.tsx
+++ src/templates/blog/blog-home-page.tsx
@@ -1,22 +1,90 @@
 import Header from "./header/header";
 import PostCard from "./post-card/post-card";
+import PostGridCard from "./post-grid-card/post-grid-card";
 
 const BlogHomePage = () => {
 	return (
 		<>
 			<Header />
-			<PostCard
-				title="Transformando seu negócio em uma loja virtual"
-				description="Se você está buscando uma maneira simples e eficaz de vender seus produtos online, o Site.Set é a solução perfeita para você. Criar uma loja virtual de sucesso nunca foi tão fácil. Com nossa plataforma intuitiva, você pode criar um site profissional para sua loja em minutos, sem precisar de conhecimentos técnicos."
-				date="20/12/24"
-				author={{
-					name: "Annette Bones",
-					role: "CEO na Anne Corp",
-					avatar: "/assets/customer01.png",
-				}}
-				imageSource="/assets/first-post.png"
-				slug="transformando-seu-negocio-em-uma-loja-virtual"
-			/>
+			<PostGridCard>
+				<PostCard
+					title="Transformando seu negócio em uma loja virtual"
+					description="Se você está buscando uma maneira simples e eficaz de vender seus produtos online, o Site.Set é a solução perfeita para você. Criar uma loja virtual de sucesso nunca foi tão fácil. Com nossa plataforma intuitiva, você pode criar um site profissional para sua loja em minutos, sem precisar de conhecimentos técnicos."
+					date="20/12/24"
+					author={{
+						name: "Annette Bones",
+						role: "CEO na Anne Corp",
+						avatar: "/assets/customer01.png",
+					}}
+					imageSource="/assets/first-post.png"
+					slug="transformando-seu-negocio-em-uma-loja-virtual"
+				/>
+
+				<PostCard
+					title="Transformando seu negócio em uma loja virtual"
+					description="Se você está buscando uma maneira simples e eficaz de vender seus produtos online, o Site.Set é a solução perfeita para você. Criar uma loja virtual de sucesso nunca foi tão fácil. Com nossa plataforma intuitiva, você pode criar um site profissional para sua loja em minutos, sem precisar de conhecimentos técnicos."
+					date="20/12/24"
+					author={{
+						name: "Annette Bones",
+						role: "CEO na Anne Corp",
+						avatar: "/assets/customer01.png",
+					}}
+					imageSource="/assets/first-post.png"
+					slug="transformando-seu-negocio-em-uma-loja-virtual"
+				/>
+
+				<PostCard
+					title="Transformando seu negócio em uma loja virtual"
+					description="Se você está buscando uma maneira simples e eficaz de vender seus produtos online, o Site.Set é a solução perfeita para você. Criar uma loja virtual de sucesso nunca foi tão fácil. Com nossa plataforma intuitiva, você pode criar um site profissional para sua loja em minutos, sem precisar de conhecimentos técnicos."
+					date="20/12/24"
+					author={{
+						name: "Annette Bones",
+						role: "CEO na Anne Corp",
+						avatar: "/assets/customer01.png",
+					}}
+					imageSource="/assets/first-post.png"
+					slug="transformando-seu-negocio-em-uma-loja-virtual"
+				/>
+
+				<PostCard
+					title="Transformando seu negócio em uma loja virtual"
+					description="Se você está buscando uma maneira simples e eficaz de vender seus produtos online, o Site.Set é a solução perfeita para você. Criar uma loja virtual de sucesso nunca foi tão fácil. Com nossa plataforma intuitiva, você pode criar um site profissional para sua loja em minutos, sem precisar de conhecimentos técnicos."
+					date="20/12/24"
+					author={{
+						name: "Annette Bones",
+						role: "CEO na Anne Corp",
+						avatar: "/assets/customer01.png",
+					}}
+					imageSource="/assets/first-post.png"
+					slug="transformando-seu-negocio-em-uma-loja-virtual"
+				/>
+
+				<PostCard
+					title="Transformando seu negócio em uma loja virtual"
+					description="Se você está buscando uma maneira simples e eficaz de vender seus produtos online, o Site.Set é a solução perfeita para você. Criar uma loja virtual de sucesso nunca foi tão fácil. Com nossa plataforma intuitiva, você pode criar um site profissional para sua loja em minutos, sem precisar de conhecimentos técnicos."
+					date="20/12/24"
+					author={{
+						name: "Annette Bones",
+						role: "CEO na Anne Corp",
+						avatar: "/assets/customer01.png",
+					}}
+					imageSource="/assets/first-post.png"
+					slug="transformando-seu-negocio-em-uma-loja-virtual"
+				/>
+
+				<PostCard
+					title="Transformando seu negócio em uma loja virtual"
+					description="Se você está buscando uma maneira simples e eficaz de vender seus produtos online, o Site.Set é a solução perfeita para você. Criar uma loja virtual de sucesso nunca foi tão fácil. Com nossa plataforma intuitiva, você pode criar um site profissional para sua loja em minutos, sem precisar de conhecimentos técnicos."
+					date="20/12/24"
+					author={{
+						name: "Annette Bones",
+						role: "CEO na Anne Corp",
+						avatar: "/assets/customer01.png",
+					}}
+					imageSource="/assets/first-post.png"
+					slug="transformando-seu-negocio-em-uma-loja-virtual"
+				/>
+			</PostGridCard>
 		</>
 	);
 };
