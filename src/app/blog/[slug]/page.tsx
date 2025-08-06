d84d6fcfaa01d1284dd112f18d5b3582a616ea5f
Mariano Capiliku <marianocapiliku@gmail.com>
2025-08-06 12:59:42 +0100

    added dynamic metatag on post list page and on individual post page, for SEO

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,13 +1,56 @@
 import { allPosts } from "contentlayer/generated";
+import type { Metadata } from "next";
 import { notFound } from "next/navigation";
 import PostP from "@/templates/blog/post/post";
+
+export const metadata: Metadata = {
+	title: "Site.set",
+	description: "Venda seus produtos como afiliado em um único lugar",
+	robots: "index, follow",
+	openGraph: {
+		title: "Site.set",
+		description: "Venda seus produtos como afiliado em um único lugar",
+		url: "https://site-blog-jade-phi.vercel.app/og-image.jpg",
+		siteName: "Site.set",
+		locale: "pt_BR",
+		type: "website",
+		images: [
+			{
+				url: "https://site-blog-jade-phi.vercel.app/og-image.jpg",
+				width: 800,
+				height: 600,
+				alt: "Site.set",
+			},
+		],
+	},
+};
 
 type BlogPostProps = {
 	params: Promise<{
 		slug: string;
 	}>;
 };
-async function PostPage({ params }: BlogPostProps) {
+export async function generateMetadata({
+	params,
+}: BlogPostProps): Promise<Metadata> {
+	const { slug } = await params;
+	const post = allPosts.find((post) => post.slug === slug);
+	if (!post) {
+		return {};
+	}
+
+	return {
+		title: post.title,
+		description: post.description,
+		authors: [{ name: post.author.name }],
+		robots: "index, follow",
+		openGraph: {
+			images: [post.image],
+		},
+	};
+}
+
+export default async function PostPage({ params }: BlogPostProps) {
 	const { slug } = await params;
 	const post = allPosts.find((post) => post.slug === slug);
 
@@ -17,5 +60,3 @@ async function PostPage({ params }: BlogPostProps) {
 
 	return <PostP post={post} />;
 }
-
-export default PostPage;
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,28 @@
 import { allPosts } from "contentlayer/generated";
+import type { Metadata } from "next";
 import BlogHomePage from "@/templates/blog/blog-home-page";
+
+export const metadata: Metadata = {
+	title: "Blog",
+	description: "Dicas e estrategias para impulsionar o seu negócio",
+	robots: "index, follow",
+	openGraph: {
+		title: "Site.set",
+		description: "Dicas e estrategias para impulsionar o seu negócior",
+		url: "https://site-blog-jade-phi.vercel.app/og-image.jpg",
+		siteName: "Site.set",
+		locale: "pt_BR",
+		type: "website",
+		images: [
+			{
+				url: "https://site-blog-jade-phi.vercel.app/og-image.jpg",
+				width: 800,
+				height: 600,
+				alt: "Site.set",
+			},
+		],
+	},
+};
 
 const Blog = () => {
 	const sortedPosts = allPosts.sort(
